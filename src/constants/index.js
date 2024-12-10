b1814d029947cefd35a3db715e8f8d1f6d51d801
Your Name <lokeshdesilla038@gmail.com>
2024-12-10 14:14:32 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,56 @@
+import project1 from "../assets/projects/project-1.avif";
+import project2 from "../assets/projects/project-2.avif";
+
+export const HERO_CONTENT = `I’m a passionate Full Stack Python Developer who loves building strong and scalable web applications. Although I’m just starting my journey, I’ve gained hands-on experience with front-end technologies like React.js and Frameworks along with back-end technologies such as MySQL, Django. I aim to use my growing skills to create innovative solutions that help businesses grow and provide great experiences for users.`;
+
+export const ABOUT_TEXT = `I am an enthusiastic full stack developer with a strong foundation in Python and a passion for creating user-friendly web applications. While I am at the beginning of my professional journey, I have developed skills in technologies such as React.js and  MySQL through hands-on projects and coursework. My curiosity about technology drives me to learn continuously and tackle new challenges. I thrive in collaborative environments and enjoy problem-solving to deliver effective solutions. Outside of coding, I love staying active, exploring new technologies, and engaging with the open-source community.`;
+
+export const EDUCATION = [
+  {
+    institution: "Sri Venkateswara College Of Engineering & Technology",
+    degree: "Bachelor of Technology",
+    fieldOfStudy: "Electrical & Electronics Engineering",
+    duration: "2024-Passedout",
+    cgpa: 7.7
+  },
+  {
+    institution: "Gnana Jyithi jr College",
+    degree: "Intermediate",
+    fieldOfStudy: "MPC",
+    duration: "2018-2020",
+    cgpa: 8.3
+  },
+  {
+    institution: "Andhra Pradesh Model School",
+    degree: "Secondary School Certificate",
+    fieldOfStudy: "SSC BOARD",
+    duration: "2018-Completed",
+    cgpa: 9.2
+  }
+];
+
+export const PROJECTS = [
+  {
+    title: "Online Grocery Shop",
+    image: project1,
+    description: "A fully functional Online Grocery Shop website with features like product cart.",
+     //link: "https://fashion-hub-clothing.netlify.app/",
+    linkName: "Visit Now",
+    technologies: ["Sqllite", "Frontend", "Django","Python"],
+  },
+  {
+    title: "Coffee Point",
+    image: project2,
+    description: "Welcome to spotify clone, a curated showcase of my best work and projects, highlighting my skills.",
+    //link: "https://coffeepnt.netlify.app/",
+    linkName: "Visit Now",
+    technologies: ["HTML", "CSS", "JavaScript","Reactjs"],
+  },
+];
+
+export const CONTACT = {
+  address: "Hyderabad, India",
+  phoneNo: "+91 6301562173",
+  email: "lokeshdesilla038@gmail.com",
+};
+
