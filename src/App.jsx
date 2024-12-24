db47a3a02dfa1598c646af2ec4eeb0899807e8d4
Your Name <lokeshdesilla038@gmail.com>
2024-12-24 13:10:08 +0530

    four commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,7 @@ import Footer from "./Components/Footer";
 const App = () => {
   return (
     <div className="overflow-x-hidden text-stone-300 antialiased">
-      {/* <div className="fixed inset-0 -z-10">
-        <div class="relative h-full w-full bg-black">
-          <div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#ff80002e_1px,transparent_1px),linear-gradient(to_bottom,#0000000a_1px,transparent_1px)] bg-[size:14px_24px]">
-            
-          </div>
-          <div class="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_500px_at_50%_300px,#ff800036,#0000)]">
-
-          </div>
-        </div>
-      </div> */}
+     
       <div>
         <Background/>
       </div>
